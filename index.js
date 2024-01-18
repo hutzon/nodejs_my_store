8d35090b7ec53113447837bac4059794a84869f0
Ottoniel Campos <hutzon4011@gmail.com>
2024-01-17 20:56:02 -0600

    mi primer commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routes');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+const whitelist = [
+  'http://localhost:8080',
+  'http://localhost:3000',
+  'http://mytest.com',
+];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+app.use(cors(options));
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, (err, res) => {
+  console.log('Mi port ' + port);
+});
